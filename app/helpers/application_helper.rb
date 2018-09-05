7fb6d32dd00f5215594f914c19b7457ec823f2ba
Ly Van Minh <lyvanminh1002@gmail.com>
2018-09-05 22:45:45 +0700

    Chapter 5: Filling in the layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def full_title page_title = ""
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = t "helpers.application_helper.text"
+
     if page_title.empty?
       base_title
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en/ do
     root "static_pages#home"
-    get "static_pages/about"
-    get "static_pages/home"
-    get "static_pages/help"
+    get  '/home', to: 'static_pages#home'
+    get  '/about', to: 'static_pages#about'
+    get  '/help', to: 'static_pages#help'
+    get  '/contact', to: 'static_pages#contact'
   end
 end
-
