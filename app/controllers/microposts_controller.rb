f03092d04eff94b2fc1e7bb260b4f88fa73f8e40
Ly Van Minh <lyvanminh1002@gmail.com>
2018-09-12 10:41:27 +0700

    Chapter 13: User microposts (Part 3)

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,9 +1,10 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: %i(create destroy)
+  before_action :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build micropost_params
-    
+
     if @micropost.save
       flash[:success] = t(".micropost_create")
       redirect_to root_url
@@ -14,11 +15,20 @@ class MicropostsController < ApplicationController
     end
   end
 
-  def destroy; end
+  def destroy
+    @micropost.destroy
+    flash[:success] = t(".micropost_deleted")
+    redirect_to request.referrer || root_url
+  end
 
   private
 
   def micropost_params
-    params.require(:micropost).permit :content
+    params.require(:micropost).permit :content, :picture
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by id: params[:id]
+    redirect_to root_url unless @micropost
   end
 end
